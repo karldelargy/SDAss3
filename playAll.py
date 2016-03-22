b5049fd32095953b5ef78e888197e6e4a400cf9a
karldelargy <karldelargy@gmail.com>
2016-03-22 19:14:58 +0000

    forgot a file

---
+++ playAll.py
@@ -0,0 +1,23 @@
+from time import sleep   
+def playAll(money, attack, playerOne): #this is everything it needs
+	if(len(playerOne['hand'])>0):
+		for x in range(0, len(playerOne['hand'])):
+		    card = playerOne['hand'].pop()
+		    playerOne['active'].append(card)
+		    money = money + card.get_money()
+		    attack = attack + card.get_attack()
+
+	print "\nYour Hand"
+	index = 0
+	for card in playerOne['hand']:
+		print "[%s] %s" % (index, card)
+		index = index + 1
+        sleep(0.5)
+	print "\nYour Active Cards"
+	for card in playerOne['active']:
+		print card
+		sleep(0.5)
+	print "\nYour Values"
+	print "Money %s, Attack %s" % (money, attack)
+
+	return (money,attack, playerOne)  ##this is whats updated
