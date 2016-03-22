2db9f922e84775978e1c612e56c00f19aef2e7bc
karldelargy <karldelargy@gmail.com>
2016-03-22 15:10:30 +0000

    more modularisation progress

--- dbc.py
+++ dbc.py
@@ -136,20 +136,7 @@ def Quit():
 	print "Exiting game..."
 	exit()
 	
-
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
+def Init():
     pO = {'name': 'player one', 'health': 30, 'deck': None, 'hand': None, 'active': None, 'handsize': 5,
                  'discard': None}
     pC = {'name': 'player computer', 'health': 30, 'deck': None, 'hand': None, 'active': None, 'handsize': 5,
@@ -180,7 +167,40 @@ if __name__ == '__main__':
     central['deck'] = deck
     central['supplement'] = supplement
     central['active'] = []
+    return(pO, pC, central)
 
+def handStrength(pO):
+    for x in range(0, len(pO['hand'])):
+        card = pO['hand'].pop()
+        pO['active'].append(card)
+        money = money + card.get_money()
+        attack = attack + card.get_attack()
+    for x in range(0, len(pO['deck'])):
+        card = pO['deck'].pop()
+        pO['active'].append(card)
+        money = money + card.get_money()
+        attack = attack + card.get_attack()
+    for x in range(0, len(pO['discard'])):
+        card = pO['discard'].pop()
+        pO['active'].append(card)
+        money = money + card.get_money()
+        attack = attack + card.get_attack()
+    pHT = money + attack
+    return (pHT)
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    pO, pC, central=Init()	
+	
     max = central['activeSize']
     count = 0
     while count < max:
@@ -406,41 +426,9 @@ if __name__ == '__main__':
             else:
             ########################################################################
             	
-            	for x in range(0, len(pO['hand'])):
-                    card = pO['hand'].pop()
-                    pO['active'].append(card)
-                    money = money + card.get_money()
-                    attack = attack + card.get_attack()
-                for x in range(0, len(pO['deck'])):
-                    card = pO['deck'].pop()
-                    pO['active'].append(card)
-                    money = money + card.get_money()
-                    attack = attack + card.get_attack()
-                for x in range(0, len(pO['discard'])):
-                    card = pO['discard'].pop()
-                    pO['active'].append(card)
-                    money = money + card.get_money()
-                    attack = attack + card.get_attack()
-                pHT = money + attack
-                money=0
-                attack=0
+                pHT=handStrength(pO)
+                pCT=handStrength(pC)
                 
-                for x in range(0, len(pC['hand'])):
-                    card = pC['hand'].pop()
-                    pC['active'].append(card)
-                    money = money + card.get_money()
-                    attack = attack + card.get_attack()
-                for x in range(0, len(pC['deck'])):
-                    card = pC['deck'].pop()
-                    pC['active'].append(card)
-                    money = money + card.get_money()
-                    attack = attack + card.get_attack()
-                for x in range(0, len(pO['discard'])):
-                    card = pC['discard'].pop()
-                    pC['active'].append(card)
-                    money = money + card.get_money()
-                    attack = attack + card.get_attack()
-                pCT = money + attack
                 
                 if pHT > pCT:
                     print "Player One Wins on Card Strength"
@@ -459,50 +447,7 @@ if __name__ == '__main__':
                 aggressive = (oT=='A')
                 
                 
-                pO = {'name': 'player one', 'health': 30, 'deck': None, 'hand': None, 'active': None,
-                             'handsize': 5,
-                             'discard': None}
-                pC = {'name': 'player computer', 'health': 30, 'deck': None, 'hand': None, 'active': None, 'handsize': 5,
-                            'discard': None}
-                central = {'name': 'central', 'active': None, 'activeSize': 5, 'supplement': None, 'deck': None}
-                sdc = [
-                                4 * [Card('Archer', (3, 0), 2)],
-                                4 * [Card('Baker', (0, 3), 2)],
-                                3 * [Card('Swordsman', (4, 0), 3)],
-                                2 * [Card('Knight', (6, 0), 5)],
-                                3 * [Card('Tailor', (0, 4), 3)],
-                                3 * [Card('Crossbowman', (4, 0), 3)],
-                                3 * [Card('Merchant', (0, 5), 4)],
-                                4 * [Card('Thug', (2, 0), 1)],
-                                4 * [Card('Thief', (1, 1), 1)],
-                                2 * [Card('Catapault', (7, 0), 6)],
-                                2 * [Card('Caravan', (1, 5), 5)],
-                                2 * [Card('Assassin', (5, 0), 4)]
-                            ]
-                playeronedeck = [8 * [Card('Serf', (0, 1), 0)],
-                                 2 * [Card('Squire', (1, 0), 0)]
-                            ]
-                pod = list(itertools.chain.from_iterable(playeronedeck))
-                pO['deck'] = pod
-                pO['hand'] = []
-                pO['discard'] = []
-                pO['active'] = []
-                playertwodeck = [
-                            8 * [Card('Serf', (0, 1), 0)],
-                            2 * [Card('Squire', (1, 0), 0)]
-                ]
-                ptd = list(itertools.chain.from_iterable(playertwodeck))
-                pC['deck'] = ptd
-                pC['hand'] = []
-                pC['discard'] = []
-                pC['active'] = []
-
-                supplement = 10 * [Card('Levy', (1, 2), 2)]
-                deck = list(itertools.chain.from_iterable(sdc))
-                random.shuffle(deck)
-                central['deck'] = deck
-                central['supplement'] = supplement
-                central['active'] = []
+                pO, pC, central=Init()
 
                 for x in range(0, central['activeSize']):
                     card = central['deck'].pop()
